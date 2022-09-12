a338c27dac5ffca3f2d329ef1edf9f0a008219a9
Vladylsav Palchykov <vladyslav.palchykov@onix-systems.com>
2022-09-12 19:30:11 +0300

    rebus autodelegation

--- rebus/autodelegation.py
+++ rebus/autodelegation.py
@@ -7,10 +7,10 @@ import time
 from subprocess import Popen, PIPE
 
 # constants
-ARCHWAY_DECIMALS = 1000000
+REBUS_DECIMALS = 1000000000000000000
 TRANSACTION_WAIT_TIME = 10
 
-class ArchwayAutodelegation():
+class RebusAutodelegation():
     def __init__( self, config_file='config.ini' ):
         # obtain the host name
         self.name = os.uname()[1]
@@ -18,10 +18,10 @@ class ArchwayAutodelegation():
         # read the config and setup the telegram
         self.read_config( config_file )
         self.setup_telegram()
-        self.setup_archway_info()
+        self.setup_rebus_info()
 
         # send the hello message
-        self.send( f'Hello from ARCHWAY Autodelegation Bot on {self.name}!' )
+        self.send( f'Hello from REBUS Autodelegation Bot on {self.name}!' )
         
     def read_config( self, config_file ):
         '''
@@ -55,32 +55,32 @@ class ArchwayAutodelegation():
         else:
             self.telegram_chat_id = None
 
-    def setup_archway_info( self ):
+    def setup_rebus_info( self ):
         '''
-        Setup archway info
+        Setup rebus info
         '''
 
         # sleep time between delegation cycles
         if "SLEEP_TIME" in os.environ:
             self.sleep_time = int(os.environ['SLEEP_TIME'])
-        elif 'sleep_time' in self.config['ARCHWAY']:
-            self.sleep_time = int(self.config['ARCHWAY']['sleep_time'])
+        elif 'sleep_time' in self.config['REBUS']:
+            self.sleep_time = int(self.config['REBUS']['sleep_time'])
         else:
             self.sleep_time = 600
         
         # bank reserve
-        if "ARCHWAY_RESERVE" in os.environ:
-            self.reserve = float(os.environ['ARCHWAY_RESERVE'])
-        elif 'reserve' in self.config['ARCHWAY']:
-            self.reserve = float(self.config['ARCHWAY']['reserve'])
+        if "REBUS_RESERVE" in os.environ:
+            self.reserve = float(os.environ['REBUS_RESERVE'])
+        elif 'reserve' in self.config['REBUS']:
+            self.reserve = float(self.config['REBUS']['reserve'])
         else:
             self.reserve = 0.1000
 
         # Prompt for the password if not in environment
-        if "ARCHWAY_PASSWORD" in os.environ:
-            self.password = os.environ['ARCHWAY_PASSWORD']
-        elif 'password' in self.config['ARCHWAY']:
-            self.password = self.config['ARCHWAY']['password']
+        if "REBUS_PASSWORD" in os.environ:
+            self.password = os.environ['REBUS_PASSWORD']
+        elif 'password' in self.config['REBUS']:
+            self.password = self.config['REBUS']['password']
         else:
             self.password = getpass.getpass("Enter the wallet password: ")
 
@@ -88,7 +88,7 @@ class ArchwayAutodelegation():
         if "CHAIN_ID" in os.environ:
             self.chain_id = os.environ['CHAIN_ID']
         else:
-            self.chain_id = self.config['ARCHWAY']['chain_id']
+            self.chain_id = self.config['REBUS']['chain_id']
 
         # wallet name
         if "WALLET_NAME" in os.environ:
@@ -96,29 +96,29 @@ class ArchwayAutodelegation():
         elif "WALLETNAME" in os.environ:
             self.wallet_name = os.environ['WALLETNAME']
         else:
-            self.wallet_name = self.config['ARCHWAY']['wallet_name']
+            self.wallet_name = self.config['REBUS']['wallet_name']
         
         # wallet and validator keys
         if "WALLET_KEY" in os.environ:
             self.wallet_key = os.environ['WALLET_KEY']
-        elif 'wallet_key' in self.config['ARCHWAY']:
-            self.wallet_key = self.config['ARCHWAY']['wallet_key']
+        elif 'wallet_key' in self.config['REBUS']:
+            self.wallet_key = self.config['REBUS']['wallet_key']
         elif "WALLET_ADDRESS" in os.environ:
             self.wallet_key = os.environ['WALLET_ADDRESS']
-        elif 'wallet_address' in self.config['ARCHWAY']:
-            self.wallet_key = self.config['ARCHWAY']['wallet_address']
+        elif 'wallet_address' in self.config['REBUS']:
+            self.wallet_key = self.config['REBUS']['wallet_address']
         else:
             print('Unable to find the wallet address in the configuration file. Exiting...')
             exit()
 
         if "VALIDATOR_KEY" in os.environ:
             self.validator_key = os.environ['VALIDATOR_KEY']
-        elif 'validator_key' in self.config['ARCHWAY']:
-            self.validator_key = self.config['ARCHWAY']['validator_key']
+        elif 'validator_key' in self.config['REBUS']:
+            self.validator_key = self.config['REBUS']['validator_key']
         elif "VALIDATOR_ADDRESS" in os.environ:
             self.validator_key = os.environ['VALIDATOR_ADDRESS']
-        elif 'validator_address' in self.config['ARCHWAY']:
-            self.validator_key = self.config['ARCHWAY']['validator_address']
+        elif 'validator_address' in self.config['REBUS']:
+            self.validator_key = self.config['REBUS']['validator_address']
         else:
             print('Unable to find the validator address in the configuration file. Exiting...')
             exit()
@@ -143,19 +143,19 @@ class ArchwayAutodelegation():
         '''
         return the share to decimal conversion
         '''
-        return float( shares ) * ( 1/ARCHWAY_DECIMALS )
+        return float( shares ) * ( 1/REBUS_DECIMALS )
 
     def decimal_to_shares( self, amount ):
         '''
         return the decimal to shares conversion
         '''
-        return int( amount * ARCHWAY_DECIMALS )
+        return int( amount * REBUS_DECIMALS )
 
     def get_balance( self ):
         '''
-        Obtain the ARCHWAY balance
+        Obtain the REBUS balance
         '''
-        proc = Popen([ f"archwayd q bank balances {self.wallet_key}" ], stdout=PIPE, shell=True)
+        proc = Popen([ f"rebusd q bank balances {self.wallet_key}" ], stdout=PIPE, shell=True)
         (out, err) = proc.communicate()
         line = self.parse_subprocess( out, 'amount' )
         balance = line.split('"')[1]
@@ -165,7 +165,7 @@ class ArchwayAutodelegation():
         '''
         Distribute the rewards from the validator and return the hash
         '''
-        child = pexpect.spawn(f"archwayd tx distribution withdraw-rewards { self.validator_key } --chain-id={ self.chain_id } --from {self.wallet_name} -y", timeout=10)
+        child = pexpect.spawn(f"rebusd tx distribution withdraw-rewards { self.validator_key } --chain-id={ self.chain_id } --from {self.wallet_name} -y", timeout=10)
         child.expect( b'Enter keyring passphrase:' ) 
         child.sendline( self.password )   
         child.expect( pexpect.EOF )                                                                                                                                     
@@ -178,7 +178,7 @@ class ArchwayAutodelegation():
         '''
         Distribute the comission for the validator and return the hash
         '''
-        child = pexpect.spawn(f"archwayd tx distribution withdraw-rewards { self.validator_key } --chain-id={ self.chain_id } --from {self.wallet_name} --commission -y", timeout=10)
+        child = pexpect.spawn(f"rebusd tx distribution withdraw-rewards { self.validator_key } --chain-id={ self.chain_id } --from {self.wallet_name} --commission -y", timeout=10)
         child.expect( b'Enter keyring passphrase:' ) 
         child.sendline( self.password )   
         child.expect( pexpect.EOF )                                                                                                                                     
@@ -191,7 +191,7 @@ class ArchwayAutodelegation():
         '''
         Delegate the amount to the validator
         '''
-        child = pexpect.spawn( f'archwayd tx staking delegate { self.validator_key } { amount }utorii --from { self.wallet_name } --chain-id { self.chain_id } -y', timeout=10)
+        child = pexpect.spawn( f'rebusd tx staking delegate { self.validator_key } { amount }utorii --from { self.wallet_name } --chain-id { self.chain_id } -y', timeout=10)
         child.expect( b'Enter keyring passphrase:' ) 
         child.sendline( self.password )   
         child.expect( pexpect.EOF )                                                                                                                                     
@@ -204,7 +204,7 @@ class ArchwayAutodelegation():
         '''
         Obtain the delegation amount for the validator
         '''
-        proc = Popen([ f"archwayd q staking delegations-to {self.validator_key} --chain-id={self.chain_id}" ], stdout=PIPE, shell=True)
+        proc = Popen([ f"rebusd q staking delegations-to {self.validator_key} --chain-id={self.chain_id}" ], stdout=PIPE, shell=True)
         (out, err) = proc.communicate()
         line = self.parse_subprocess( out, 'shares' )
         balance = self.shares_to_decimal( line.split('"')[1].split(".")[0]) 
@@ -258,8 +258,8 @@ def parse_arguments( ):
 args = parse_arguments()
 
 # Create the object
-archway_bot = ArchwayAutodelegation( args.config )
+rebus_bot = RebusAutodelegation( args.config )
 
 # run periodic delegation cycle
 while True:
-    archway_bot.delegation_cycle()
+    rebus_bot.delegation_cycle()
